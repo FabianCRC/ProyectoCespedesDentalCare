ba05798bc6138379d9b9dc393a0cad813811639a
LAPTOP-NTJ8C71K\Fabian <rojascamachof@gmail.com>
2021-02-10 13:24:58 -0600

    Update RegisterController.php
    correcion de ruta

--- ProyectoCespedesDentalCare/app/Http/Controllers/Auth/RegisterController.php
+++ ProyectoCespedesDentalCare/app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
