b1c99abbafaedc97f536c630b05c6b9425b9f117
LAPTOP-NTJ8C71K\Fabian <rojascamachof@gmail.com>
2021-02-16 21:33:23 -0600

    Correcion pie de pagina
    Correccion al pie de pagina en el layout

--- ProyectoCespedesDentalCare/resources/views/layout.blade.php
+++ ProyectoCespedesDentalCare/resources/views/layout.blade.php
@@ -97,7 +97,7 @@
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Cespedes Dental Care</div>
+                        <div class="text-muted">Cespedes Dental Care Digital</div>
                         <div>
                             <a href="#"></a>
                             &middot;
