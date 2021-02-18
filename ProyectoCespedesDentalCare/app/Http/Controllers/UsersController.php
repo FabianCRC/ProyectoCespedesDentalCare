0ceef1d9a6fb9748874d1b04073343bbbdc45eee
LAPTOP-NTJ8C71K\Fabian <rojascamachof@gmail.com>
2021-02-18 09:42:11 -0600

    Encripcion a las contraseñas de los usuarios
    Encripcion a las contraseñas de los usuarios ya en los metodos update y store

--- ProyectoCespedesDentalCare/app/Http/Controllers/UsersController.php
+++ ProyectoCespedesDentalCare/app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
+
 class UsersController extends Controller
 { /**
     * Display a listing of the resource.
@@ -33,33 +35,37 @@ class UsersController extends Controller
     * @param  \Illuminate\Http\Request  $request
     * @return \Illuminate\Http\Response
     */
-   public function store(Request $request)
-   {
-    $request->validate([
-        'name' => 'required',
-        'apellido' => 'required',
-        'cedula' => 'required',
-        'imagen' => 'required',
-        'telefono' => 'required',
-        'idRol' => 'required',
-        'email' => 'required',
-        'password' => 'required',]);
-
-
-       //$datosUsuario=request()->all();
-       $datosUsuario=request()->except('_token');
-
-       if($request->hasFile('imagen')){
-
-           $datosUsuario['imagen']=$request->file('imagen')->store('uploads','public');
-       }
-
-       users::insert($datosUsuario);
-      // return response()->json($datosUsuario);
-       //return $datosUsuario;
-
-       return redirect('Usuarios');
-   }
+    public function store(Request $request)
+    {
+        $request->request->add(['password'=> Hash::make($request->input('password'))
+        ]);
+     $request->validate([
+         'name' => 'required',
+         'apellido' => 'required',
+         'cedula' => 'required',
+         'imagen' => 'required',
+         'telefono' => 'required',
+         'idRol' => 'required',
+         'email' => 'required',
+         'password' => 'required',]);
+         
+     
+ 
+        //$datosUsuario=request()->all();
+        $datosUsuario=request()->except('_token');
+ 
+        if($request->hasFile('imagen')){
+ 
+            $datosUsuario['imagen']=$request->file('imagen')->store('uploads','public');
+        }
+ 
+        users::insert($datosUsuario);
+       // return response()->json($datosUsuario);
+        //return $datosUsuario;
+ $request->password = bcrypt($request->password);
+        return redirect('Usuarios');
+    }
+ 
 
    /**
     * Display the specified resource.
@@ -94,6 +100,8 @@ class UsersController extends Controller
     */
    public function update(Request $request, $id)
    {
+    $request->request->add(['password'=> Hash::make($request->input('password'))
+    ]);
        $datosUsuario=request()->except(['_token','_method']);
 
 
